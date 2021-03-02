66fb63dd34f9331f5b69f41d4caaf552151e89da
Jenkin <jiancunge.com@gmail.com>
2021-03-02 23:58:05 +0800

    feat: added default Avatar

--- src/components/Advance/index.js
+++ src/components/Advance/index.js
@@ -1,22 +1,27 @@
-import React from 'react'
-import face1 from '../../assets/images/face-male-1.jpg'
-import StyledAvatar, { AvatarClip, AvatarImage, StatusIcon } from './style'
+import React from "react";
+import face1 from "../../assets/images/face-male-1.jpg";
+import StyledAvatar, { AvatarClip, AvatarImage, StatusIcon } from "./style";
 // import PropTypes from 'prop-types'
 
-function Avatar(props) {
-    return (
-        <StyledAvatar>
-            <StatusIcon></StatusIcon>
-            <AvatarClip>
-                <AvatarImage src={face1} alt="" />
-            </AvatarClip>
-        </StyledAvatar>
-    )
+function Avatar({
+  src,
+  size = "48px",
+  status,
+  statusIconSize = "8px",
+  ...rest
+}) {
+  return (
+    <StyledAvatar {...rest}>
+      {status && (
+        <StatusIcon status={status} size={statusIconSize}></StatusIcon>
+      )}
+      <AvatarClip>
+        <AvatarImage src={face1} alt="" />
+      </AvatarClip>
+    </StyledAvatar>
+  );
 }
 
-Avatar.propTypes = {
+Avatar.propTypes = {};
 
-}
-
-export default Avatar
-
+export default Avatar;
--- src/components/Advance/style.js
+++ src/components/Advance/style.js
@@ -1,52 +1,48 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from "styled-components";
 
-const circleMixinFunc = color => css`
-    content: '';
-    display: block;
-    position: absolute;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background-color: ${color};
-`
+const circleMixinFunc = (color) => css`
+  content: "";
+  display: block;
+  position: absolute;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: ${color};
+`;
 
 const StyledAvatar = styled.div`
-    position: relative;
-`
+  position: relative;
+`;
 const StatusIcon = styled.div`
-    position: absolute;
-    top: 4px;
-    left: 2px;
+  position: absolute;
+  top: 4px;
+  left: 2px;
 
-    // 大圆圈
-    &::before {
-        ${circleMixinFunc('white')}
-        transform: scale(2);
-    }
+  // 大圆圈
+  &::before {
+    ${circleMixinFunc("white")}
+    transform: scale(2);
+  }
 
-    // 小圆圈
-    &::after {
-        ${({ theme }) => circleMixinFunc(theme.green)}
-    }
-`
+  // 小圆圈
+  &::after {
+    ${({ theme }) => circleMixinFunc(theme.green)}
+  }
+`;
 const AvatarClip = styled.div`
-    width: 48px;
-    height: 48px;
-    border-radius: 50%;
-    overflow: hidden;
-`
+  width: ${({ size }) => size};
+  height: ${({ size }) => size};
+  border-radius: 50%;
+  overflow: hidden;
+`;
 
 const AvatarImage = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: center;
-`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+`;
 
-export default StyledAvatar
+export default StyledAvatar;
 
-export {
-    StatusIcon,
-    AvatarClip,
-    AvatarImage,
-}
+export { StatusIcon, AvatarClip, AvatarImage };
